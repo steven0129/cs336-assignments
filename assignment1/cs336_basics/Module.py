714b72e06633f9a1a2b840a5549d78e4ee925655
steven0129 <h3584935@gmail.com>
2026-08-30 23:38:04 +0000

    Reduce std of Linear layer

--- assignment1/cs336_basics/Module.py
+++ assignment1/cs336_basics/Module.py
@@ -10,7 +10,14 @@ class Linear(nn.Module):
     def __init__(self, in_features, out_features, device=None, dtype=None):
         super(Linear, self).__init__()
         self.weight = nn.Parameter(torch.empty((out_features, in_features), device=device, dtype=dtype))
-        torch.nn.init.trunc_normal_(self.weight)
+        std = math.sqrt(2 / (in_features + out_features))
+        torch.nn.init.trunc_normal_(
+            self.weight,
+            mean=0.0,
+            std=std,
+            a=-3 * std,
+            b=3 * std,
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return einsum(x, self.weight, "... d_in, d_out d_in -> ... d_out")
