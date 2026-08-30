bc514ad520c7e78887b29df2a49d68f99fdaf6f4
steven0129 <h3584935@gmail.com>
2026-08-30 23:53:12 +0000

    Save checkpoint

--- assignment1/scripts/train.py
+++ assignment1/scripts/train.py
@@ -1,9 +1,11 @@
 import argparse
 import torch
 import numpy as np
+import os
 from cs336_basics.Module import AdamW, CrossEntropyLoss
 from cs336_basics.Module import TransformerLM
 from cs336_basics.Data import Loader
+from cs336_basics.Checkpoint import save_checkpoint
 
 torch.manual_seed(42)
 
@@ -13,9 +15,14 @@ if __name__ == '__main__':
     parser.add_argument('--batch-size', default=5, type=int)
     parser.add_argument('--context-length', default=256, type=int)
     parser.add_argument('--device', default='cuda')
+    parser.add_argument('--save-every', default=1000, type=int)
+    parser.add_argument('--log-dir', default='logs')
     args = parser.parse_args()
 
-    
+
+    if not os.path.isdir(args.log_dir):
+        os.makedirs(args.log_dir)
+
     dataset = np.memmap(
         f'{args.dataset_path}/TinyStoriesV2-GPT4-train.txt.npy',
         dtype=np.uint16,
@@ -64,4 +71,9 @@ if __name__ == '__main__':
         if step % 10 == 0:
             print(
                 f"step={step:6d}/{len(data_loader)}, loss={loss.item():.4f}"
+            )
+
+        if step % args.save_every == 0:
+            save_checkpoint(
+                model, optimizer, step, f'{args.log_dir}/model{step}.pt'
             )
